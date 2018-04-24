8892f0c4237acc2c59eda3eb4fc8071819e77e42
ahmad <ahmad@navlys.com>
2018-04-24 18:11:37 -0400

    init

---
+++ main.js
@@ -0,0 +1,29 @@
+// const allTweets = [...document.querySelectorAll('.TweetTextSize')];
+const allTweets = [document.querySelector('.TweetTextSize')];
+allTweets.map(async function (p) {
+  const url = `https://apis.paralleldots.com/v3/sentiment?text=${p.innerText}&api_key=NOT_TODAY_MY_FRIENDS`;
+  const response = await fetch(url, { method:'POST' }).then(res => res.json());
+  const colors = {
+    positive: 'green',
+    neutral: 'dodgerblue',
+    negative: 'red'
+  }
+
+  const getValuesInOrder = [];
+
+  for (let key in response.probabilities) {
+    response.probabilities.hasOwnProperty(key) 
+      ? getValuesInOrder.push([key, response.probabilities[key]]) 
+      : null;
+  }
+
+  getValuesInOrder.sort((a, b) => b[1] - a[1]);
+
+  const ul = document.createElement("div");
+  getValuesInOrder.forEach(set => {
+    ul.innerHTML += `<span style="color: ${colors[set[0]]};font-weight: ${response.sentiment === set[0] ? 'bold' : undefined};">${set[0]} ${set[1]} </span>`
+  });
+
+  p.appendChild(ul);
+});
+
---
+++ popup.js
@@ -0,0 +1,26 @@
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function(element) {
+  const allTweets = [...document.querySelectorAll('.TweetTextSize')];
+  allTweets.map(p => p.innerHTML += '<p>owo</p>');
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {
+        file: 'main.js'
+      }
+      // {code: ` [...document.querySelectorAll('.TweetTextSize')].map(p => p.innerHTML += '<p>owo</p>') `}
+    );
+  });
+  // let color = element.target.value;
+  //  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  //   chrome.tabs.executeScript(
+  //     tabs[0].id,
+  //     {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  // });
+};
